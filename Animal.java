501d8e131d9a5bad7481684c7c9ca1dc3594dc29
Taweshal Dev Thakur <143280866+taweshaldev99@users.noreply.github.com>
2024-09-17 08:08:38 +0545

    Inheritance_01 - 5

---
+++ Animal.java
@@ -0,0 +1,17 @@
+class Animal {
+String color = "white" ;
+}
+class Dog extends Animal{
+    String color = "black";
+    void printColor(){
+        System.out.println(color);
+        System.out.println(super.color);
+    }
+}
+class SuperDemo{
+    public static void main(String[] args){
+        Dog d = new Dog();
+        d.printColor();
+    }
+}
+
