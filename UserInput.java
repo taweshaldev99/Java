117f89e595daef151e79da662387d9d04d35bd6f
Taweshal Dev Thakur <143280866+taweshaldev99@users.noreply.github.com>
2024-09-17 08:04:13 +0545

    Taking user input

---
+++ UserInput.java
@@ -0,0 +1,11 @@
+package Input;
+import java.util.*;
+public class UserInput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your Name: ");
+        String name= sc.nextLine();
+        System.out.println("Your name is " +name);
+    }
+
+}
